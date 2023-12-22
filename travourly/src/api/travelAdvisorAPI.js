0a8fff3d6a42d7bb92475faaaed91722a0415f49
Sulaimon Olanrewaju <sirtunx.hokyky@gmail.com>
2023-12-22 18:10:42 +0100

    first commit

---
+++ travourly/src/api/travelAdvisorAPI.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+// const options = {
+//   params: {
+//     bl_latitude: '11.847676',
+//     tr_latitude: '12.838442',
+//     bl_longitude: '109.095887',
+//     tr_longitude: '109.149359'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '',
+//     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+//   }
+// };
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://open-weather13.p.rapidapi.com/city/latlon/30.438/-89.1028', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': 'open-weather13.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
